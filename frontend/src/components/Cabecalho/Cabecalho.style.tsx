d7235e6f73ed2201264beda022e882e587515802
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-10-20 13:51:08 -0300

    feat: adiciona componente cabecalho e imgs

---
+++ frontend/src/components/Cabecalho/Cabecalho.style.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material'
+
+export const CabecalhoContainer = styled('header')`
+    width: 230px;
+`;
+
+export const Logo = styled('img')`
+    width: 100%;
+`;
---
+++ frontend/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CabecalhoContainer } from './Cabecalho.style';
+import { Logo } from './Cabecalho.style';
+
+const Cabecalho = () => {
+    return (
+        <CabecalhoContainer>
+            <div>
+                <Logo src='/imgs/myteacher.png'/>
+            </div>
+
+            <p>Encontre o professor perfeito</p>
+        </CabecalhoContainer>
+    )
+}
+
+export default Cabecalho;
