44fb4ddac51442b22005f117204757a00fc66fd9
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-10-25 19:25:03 -0300

    feat: adiciona os estilos da Lista

--- frontend/src/components/Lista/Lista.tsx
+++ frontend/src/components/Lista/Lista.tsx
@@ -1,5 +1,45 @@
+import { Button } from "@mui/material"
+import { Descricao, Foto, Info, ItemLista, ListaStyled, Nome, Valor } from "./Lista.style"
+
 export const Lista = () => {
   return (
-    <div>Lista</div>
+    <ListaStyled>
+        <ItemLista>
+            <Foto src="https://github.com/cxavier6.png"></Foto>
+            <Info>
+                <Nome>Camila Reis</Nome>
+                <Valor>R$ 100,00 por hora</Valor>
+                <Descricao>Aulas de programação</Descricao>
+                <Button sx={{ width: '70%'}}>Marcar aula com Camila</Button>
+            </Info>
+        </ItemLista>
+        <ItemLista>
+            <Foto src="https://github.com/cxavier6.png"></Foto>
+            <Info>
+                <Nome>Camila Reis</Nome>
+                <Valor>R$ 100,00 por hora</Valor>
+                <Descricao>Aulas de programação</Descricao>
+                <Button sx={{ width: '70%'}}>Marcar aula com Camila</Button>
+            </Info>
+        </ItemLista>
+        <ItemLista>
+            <Foto src="https://github.com/cxavier6.png"></Foto>
+            <Info>
+                <Nome>Camila Reis</Nome>
+                <Valor>R$ 100,00 por hora</Valor>
+                <Descricao>Aulas de programação</Descricao>
+                <Button sx={{ width: '70%'}}>Marcar aula com Camila</Button>
+            </Info>
+        </ItemLista>
+        <ItemLista>
+            <Foto src="https://github.com/cxavier6.png"></Foto>
+            <Info>
+                <Nome>Camila Reis</Nome>
+                <Valor>R$ 100,00 por hora</Valor>
+                <Descricao>Aulas de programação</Descricao>
+                <Button sx={{ width: '70%'}}>Marcar aula com Camila</Button>
+            </Info>
+        </ItemLista>
+    </ListaStyled>
   )
 }
