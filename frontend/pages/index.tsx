860be4949d73773959a466f931d890c38f4cd593
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-10-26 10:49:40 -0300

    feat: adiciona lista de professores

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,44 @@
 import { Box } from '@mui/material'
 import type { NextPage } from 'next'
+import { Professor } from '../src/@types/professor'
 import { Lista } from '../src/components/Lista/Lista'
 
 
 const Home: NextPage = () => {
+  const professores: Professor[] = [
+    {
+      id: 1,
+      nome: "Professor 1",
+      foto: "https://github.com/cxavier6.png",
+      descricao: "Descrição do professor 1",
+      valor_hora: 100
+    },
+    {
+      id: 2,
+      nome: "Professor 2",
+      foto: "https://github.com/cxavier6.png",
+      descricao: "Descrição do professor 2",
+      valor_hora: 200
+    },
+    {
+      id: 3,
+      nome: "Professor 3",
+      foto: "https://github.com/cxavier6.png",
+      descricao: "Descrição do professor 3",
+      valor_hora: 300
+    },
+    {
+      id: 4,
+      nome: "Professor 4",
+      foto: "https://github.com/cxavier6.png",
+      descricao: "Descrição do professor 4",
+      valor_hora: 400
+    },
+  ]
+  
   return (
   <Box sx={{ backgroudColor: 'secondary.main'}}>
-    <Lista/>
+    <Lista professores={professores}/>
   </Box> 
   )
 }
