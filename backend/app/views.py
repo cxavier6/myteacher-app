7224bf728b4f133284c662cc01ffc34d2c2d69ff
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-11-03 11:54:07 -0300

    refactor: corrige typo na classe homeapiview

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,5 +2,5 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class HomeApiView(APIView):
-    def get(slef, request, format=None):
+    def get(self, request, format=None):
         return Response({"nome": "Camila Reis", "idade": 24}, status=200)
