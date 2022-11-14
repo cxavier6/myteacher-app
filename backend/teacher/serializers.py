c6354e6dee6bdecb9f3ea1c61f9df09a7e302140
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-11-13 21:27:27 -0300

    feat: adiciona serializers

---
+++ backend/teacher/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from rest_framework import serializers
+from teacher.models import Professor
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
